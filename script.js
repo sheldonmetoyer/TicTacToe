98c6153d9dd570fea8089bce5828145d373f4337
sheldonmetoyer <sheldon.metoyer@gmail.com>
2024-04-29 16:41:21 -0500

    Have gameboard completed and players created.

--- script.js
+++ script.js
@@ -1,5 +1,57 @@
-const funct = function() {
-    console.log("Hello from a function!");
+const Gameboard = (function (){
+    const board = [];
+
+    const createBoard = function() {
+        const rows = 3;
+        const cols = 3;
+
+        for (i = 0; i < rows; i++) {
+            board.push([]);
+            for(j = 0; j < cols; j++){
+                board[i].push(`${i},${j}`);
+            }
+        }
+    }
+
+    const showGameBoard = function() {
+        createBoard();
+        console.log(board);
+        return board;
+    }
+
+    return {showGameBoard};
+    
+})();
+
+const createPlayer = (name, marker) => {
+    let playerName = name;
+    let playerMarker = marker;
+
+    return {playerName, playerMarker}
 }
 
-funct();
+const GameController = (function (){
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver = false;
+
+    const start = () => {
+        players = [
+            createPlayer('playerOne', 'x'),
+            createPlayer('playerTwo', 'O')
+        ];
+        currentPlayerIndex = 0;
+        gameOver = false;
+        console.log(players);
+        Gameboard.showGameBoard();
+    }
+
+    return {start}
+})();
+
+
+
+
+
+
+
